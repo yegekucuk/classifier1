74118aaa2ad1d0d121183effd0b7d2fc81dc0b0e
yegekucuk <eegekucuk@hotmail.com>
2024-05-21 23:43:06 +0300

    Update keras_classifier1.py

--- keras_classifier1.py
+++ keras_classifier1.py
@@ -2,9 +2,6 @@ from keras.preprocessing.image import image_utils
 import numpy as np
 
 def load_image(file_path):
-    # Getting name of input image
-    img_name = input("Please enter the full name of the image: ")
-
     # Loading the image
     img = image_utils.load_img(file_path, target_size=(64,64))
     img = image_utils.img_to_array(img)
