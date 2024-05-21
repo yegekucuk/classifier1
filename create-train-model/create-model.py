b51e0c0dbe38ad39c2e946f2d8a5efef0f53a2dc
yegekucuk <eegekucuk@hotmail.com>
2024-05-22 01:55:32 +0300

    Updated and refit the Model

--- create-train-model/create-model.py
+++ create-train-model/create-model.py
@@ -5,7 +5,7 @@ from keras.models import Sequential
 model = Sequential()
 
 # Creating conv CNN layers
-model.add(Conv2D(32, (3, 3), input_shape=(64, 64, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), input_shape=(64, 64, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Conv2D(64, (3, 3), activation='relu'))
@@ -14,7 +14,7 @@ model.add(MaxPooling2D(pool_size=(2, 2)))
 # Flattening and adding Dense layers
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))  # Çıktı katmanı, insan veya değil olarak tek bir çıktı olacak
+model.add(Dense(1, activation='sigmoid'))
 
 # Compiling the model
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
--- create-train-model/train-model.py
+++ create-train-model/train-model.py
@@ -1,25 +1,43 @@
-from keras.models import load_model, Sequential
 from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential, load_model
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
-# Creating the image generators for training and testing
-train_gen = ImageDataGenerator(rescale = 1./255, shear_range = 0.2, zoom_range = 0.2, horizontal_flip = True)
-test_gen = ImageDataGenerator(rescale = 1./255)
+# Create an instance of ImageDataGenerator for training and testing
+train_gen = ImageDataGenerator(rescale=1./255)
+test_gen = ImageDataGenerator(rescale=1./255)
 
-# Generating the training and testing datasets using the generators
-train_set = train_gen.flow_from_directory("dataset", target_size = (64,64), batch_size = 1, class_mode = "binary")
-test_set1 = test_gen.flow_from_directory("test-set_preprocessed", target_size = (64,64), batch_size = 1, class_mode = "binary")
+# Load training data
+train_set = train_gen.flow_from_directory(
+    "dataset", 
+    target_size=(64, 64), 
+    batch_size=1, 
+    class_mode="binary"
+)
 
+# Load test data
+test_set = test_gen.flow_from_directory(
+    "testset", 
+    target_size=(64, 64), 
+    batch_size=1, 
+    class_mode="binary"
+)
 
-print(train_set.class_indices)
-print(test_set1.class_indices)
-raise KeyError
+# Determine the number of steps per epoch
+steps_per_epoch = train_set.samples // train_set.batch_size
+validation_steps = test_set.samples // test_set.batch_size
 
 # Loading the model
 model: Sequential
 model = load_model("model_untrained.h5")
 
 # Fitting the model
-model.fit_generator(train_set,steps_per_epoch=11987,epochs=2,validation_data=test_set1,validation_steps=120)
+model.fit(
+    train_set,
+    steps_per_epoch=steps_per_epoch,
+    epochs=10,
+    validation_data=test_set,
+    validation_steps=validation_steps
+)
 
 # Saving the model
 model.save("model_trained.h5")
